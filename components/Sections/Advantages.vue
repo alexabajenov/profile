6f9c73d3b3ee10462ddcb6713ee157e601bba755
BajenovJsDeveloper <bajenovcpm433@yandex.ru>
2022-05-15 00:56:55 +0300

    add advantage

---
+++ components/Sections/Advantages.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :class="$style.container">
+    <p :class="$style.text">what my advantages?</p>
+    <h2 :class="$style.header">My <span>Advantages</span></h2>
+  </div>
+</template>
+
+<script>
+
+export default ({
+  // 
+})
+</script>
+
+<style module lang="scss">
+.container {
+  padding: 60px;
+  .text {
+    position: relative;
+    top: -1rem;
+  }
+  .header {
+    > span {
+      color: var(--second-text-color);
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,20 @@
   <div>
     <UserSection />
     <AboutSection />
+    <Advantages />
   </div>
 </template>
 
 <script>
 import UserSection from "~/components/Sections/UserSection.vue"
 import AboutSection from "~/components/Sections/AboutSection.vue"
+import Advantages from "~/components/Sections/Advantages.vue"
 export default {
   name: 'ProfilePage',
   components: {
     UserSection,
     AboutSection,
+    Advantages,
   }
 }
 </script>
